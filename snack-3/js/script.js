136d041f9d760d0d91081325966556d47646bddf
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-04 12:45:30 +0100

    snack-3 completato

---
+++ snack-3/js/script.js
@@ -0,0 +1,10 @@
+var nomi = ["Gianluca", "Sergio", "Giuseppe", "Franco", "Gianni", "Alfio", "Armando", "Italo", "Ugo", "Bettino"], cognomi = ["Spampalino", "Kellogs", "Medusa", "Fantozzi", "Craxi", "Trapani", "Kennedy", "Cocchi", "Lenzi", "Savini"], index = 0, nomiRandom = [];
+
+for ( var i = 0; i < 3; i++ ) {
+  index = Math.floor( Math.random() * 10 );
+  nomiRandom[i] = nomi[index];
+  index = Math.floor( Math.random() * 10 );
+  nomiRandom[i] += " " + cognomi[index];
+}
+
+console.log(nomiRandom);
