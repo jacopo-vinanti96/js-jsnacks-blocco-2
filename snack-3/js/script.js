e4856c3a79a65cdb36890383f7d8df995b9a9fe0
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-04 12:59:43 +0100

    snack-3 funzione rimozione fix

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,12 +1,13 @@
-var nomi = ["Gianluca", "Sergio", "Giuseppe", "Franco", "Gianni", "Alfio", "Armando", "Italo", "Ugo", "Bettino"], cognomi = ["Spampalino", "Kellogs", "Medusa", "Fantozzi", "Craxi", "Trapani", "Kennedy", "Cocchi", "Lenzi", "Savini"], index = 0, nomiRandom = [];
+var nomi = ["Gianluca", "Sergio", "Giuseppe", "Franco", "Gianni", "Alfio", "Armando", "Italo", "Ugo", "Bettino"], cognomi = ["Spampalino", "Kellogs", "Medusa", "Fantozzi", "Craxi", "Trapani", "Kennedy", "Cocchi", "Lenzi", "Savini"], index = 0, nomiRandom = [], numStringhe = 10;
 
 for ( var i = 0; i < 3; i++ ) {
-  index = Math.floor( Math.random() * 10 );
+  index = Math.floor( Math.random() * numStringhe );
   nomiRandom[i] = nomi[index];
   nomi.pop(nomi[index]);
-  index = Math.floor( Math.random() * 10 );
+  index = Math.floor( Math.random() * numStringhe );
   nomiRandom[i] += " " + cognomi[index];
   cognomi.pop(cognomi[index]);
+  numStringhe--;
 }
 
 console.log(nomiRandom);
